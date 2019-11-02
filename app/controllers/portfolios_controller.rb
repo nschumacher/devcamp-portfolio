247c1c8f11bb671d8b8965c1b1833273bcac98e9
Nick Schumacher <nschumacher@appirio.com>
2019-11-02 16:11:44 -0400

    Integrated edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,12 +10,29 @@ class PortfoliosController < ApplicationController
 
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    
+
         respond_to do |format|
-          if @portfolio_item.save
+            if @portfolio_item.save
             format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-          else
+            else
             format.html { render :new }
+            end
+        end
+    end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id]) #looks in the params of the uri for the id param
+
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id]) #looks in the params of the uri for the id param
+
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+          else
+            format.html { render :edit }
           end
         end
       end
