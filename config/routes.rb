1b4baacb4c8614ac9485e3e9c382d5e255c7d06c
Nick Schumacher <nschumacher@appirio.com>
2019-11-02 17:05:41 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   # Below are for static pages that don't have to pull anything from the db
   #get 'pages/about' this is the same as the one below it
