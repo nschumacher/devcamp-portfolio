b08eb73e4538761678478abc2565bea7d524ce18
Nick Schumacher <nschumacher@appirio.com>
2019-11-02 16:55:56 -0400

    Implemented custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
+  # Below are for static pages that don't have to pull anything from the db
+  #get 'pages/about' this is the same as the one below it
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact' # this lets you put whatever you want on the left and it'll map to the path (controller action) on the right
+
+
   resources :blogs
+
+  root to: 'pages#home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
