7f1aa9f398ac7dd05f76c87d39e46c33c3eca749
Nick Schumacher <nschumacher@appirio.com>
2019-11-18 16:50:19 -0500

    Implemented imgae defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ruby_on_rails_porfolio_items, -> { where(subtitle: "Ruby on Rails") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "https://via.placeholder.com/150",
+        self.thumb_image ||= "https://via.placeholder.com/150x200"
+    end
 end
